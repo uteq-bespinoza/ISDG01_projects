b90fa379f85440a807d7dfd008983bceb7fed090
Bryan Espinoza <bryan.ezpinoza@uteq.edu.mx>
2021-09-17 20:23:06 -0500

    Primera version
    Contador de Xamarin

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        int _count;
+        string _message;
+        Button button1;
+        public MainPage()
+        {
+            InitializeComponent();
+            _count = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (button1 == null)
+                button1 = ((Button)sender);
+
+            _count++;
+            _message = "You clicked me {0} times";
+            button1.Text = string.Format(_message, _count);
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            _message = "Click me";
+            if (button1 != null)
+            {
+                _count = 0;
+                button1.Text = _message;
+            }
+        }
+    }
+}
